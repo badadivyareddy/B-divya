cde41c37a84715afc43bfa8d350cb4bdc1ed9197
badadivyareddy <40793752+badadivyareddy@users.noreply.github.com>
2018-07-04 10:53:05 +0530

    Rename d.py to 1.py

---
+++ 1.py
@@ -0,0 +1,9 @@
+num = int(input("Enter the number:")
+if(num>0):
+  print("Positive number")
+elif(num<0):
+  print("Negative number")
+elif(num==0):
+  print("Number is 0")
+else:
+  print("Invalid input")
