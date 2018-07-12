7fc7c0367b9079304210f5158348c6563e7b6aa9
badadivyareddy <40793752+badadivyareddy@users.noreply.github.com>
2018-07-12 11:35:30 +0530

    Create numeric.py

---
+++ numeric.py
@@ -0,0 +1,11 @@
+String=raw_input(enter string)
+count1=0
+count2=0
+for i in a:
+	if(i.isdigit()):
+count1=count1+1
+count2=count2+1
+print("the number of digits is:")
+print(count1)
+print("the number of characters is:")
+print(count2)
